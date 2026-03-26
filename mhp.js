663fe2673685f15670f6c99588105a7b14b1ffa5
WOONG <lapolaebseu@wonwoong.local>
2026-03-27 08:27:47 +0900

    debug: log auth response

--- mhp.js
+++ mhp.js
@@ -17,6 +17,7 @@ async function getToken() {
   const res = await axios.post(`${BASE}/auth`, form, {
     headers: { actor: 'mhp.console', ...form.getHeaders() }
   });
+  console.log('[MHP] auth response:', JSON.stringify(res.data));
   _token = res.data.data.accessToken;
   _tokenExpiry = Date.now() + 55 * 60 * 1000;
   return _token;
